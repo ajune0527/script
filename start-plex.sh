ab728ac15b4bb25cd5a592c93690062709816325
jiangwei.wang <jiangwei.wang@verystar.cn>
2024-03-20 17:06:00 +0800

    add

--- start-plex.sh
+++ start-plex.sh
@@ -1,14 +1,15 @@
+# https://www.plex.tv/claim/ 获取claim
 docker run \
--d \
---name plex \
---network=host \
---restart=always \
--e PUID=0 \
--e PGID=0 \
--e TZ="Asia/Shanghai" \
--e PLEX_CLAIM="" \ # https://www.plex.tv/claim/ 获取claim
--h plex \
--v /mnt/download/media/plex/config:/config \
--v /mnt/download/media/plex/transcode:/transcode \
--v /mnt/download/media:/data \
-linuxserver/plex
+  -d \
+  --name plex \
+  --network=host \
+  --restart=always \
+  -e PUID=0 \
+  -e PGID=0 \
+  -e TZ="Asia/Shanghai" \
+  -e PLEX_CLAIM="" \
+  -h plex \
+  -v /mnt/download/media/plex/config:/config \
+  -v /mnt/download/media/plex/transcode:/transcode \
+  -v /mnt/download/media:/data \
+  linuxserver/plex
